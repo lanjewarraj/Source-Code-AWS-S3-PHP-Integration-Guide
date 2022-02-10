5b7865e686a31ca33d1cd7b50b7feb3dd908e5cd
raj lanjewar <55983199+lanjewarraj@users.noreply.github.com>
2022-02-10 11:12:41 +0530

    Update delete_s3_object.php

--- delete_s3_object.php
+++ delete_s3_object.php
@@ -1,10 +1,9 @@
 <?php
 require('config.php');
     $bucket = 'put-your-source-bucket-name'; //bucket name that contains your files/content
-    $folder ='aws-s3-folder-name-inside-your-file-store'; 
-    //like myfolder/
-    $keyname = 'file-name'; 
-    //like if your file name is flower.jpg then put this name in the place of keyname(that means the path of flower.jpg file is myfolder/flower.jpg)
+    $folder ='aws-s3-folder-name-inside-your-file-store'; //for example  myfolder/
+    
+    $keyname = 'file-name'; //for example if your file name is flower.jpg then put this name in the place of keyname(that means the path of flower.jpg file is myfolder/flower.jpg)
 
 $result = $s3->deleteObject([
     'Bucket' => $bucket,
