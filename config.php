15dc6402b609272d28872682a7d7fc9ecff83bdf
raj lanjewar <55983199+lanjewarraj@users.noreply.github.com>
2022-02-10 11:11:13 +0530

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $s3Client = new S3Client([
         'profile' => 'default',
         'version' => 'latest',
         'region' => 'us-east-2',
-        //Below I provide hard coded credentials(not recomended), see best practice for providing credentials to your application https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials_hardcoded.html
+        //Below I provided hard coded credentials(not recomended), see best practice for providing credentials to your application https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials_hardcoded.html
         'credentials' => [
             'key'    => 'put-your-access-key-id',
             'secret' => 'put-your-secret-access-key',
